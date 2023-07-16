2885a1e51255bd92026ab20fa9e0f94c06c65c1b
cae <eduardokae01@hotmail.com>
2023-07-16 19:05:50 -0300

    feat: add exemplos

---
+++ webStorage/webStorage.exemplos.js
@@ -0,0 +1,6 @@
+// localStorage.length => mostra quantas entradas existem no webStorage
+// localStorage.setItem('chave', 'valor') => adiciona uma entrada no webStorage
+// localStorage.getItem('chave') => resgata uma entrada com a chave selecionada, se não existir retorna null
+// localStorage.removeItem('chave') => deleta a entrada com a chave selecionada
+// JSON.stringfy('objeto/array para transformar')
+// JSON.parse(localStorage.getItem('chave')) => transforma o obj/array transformado em string de volta ao estado original
