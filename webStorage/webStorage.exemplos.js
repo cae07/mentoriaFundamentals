8fec50f03862ab8d2d1e0a8960aaf08bec003fed
cae <eduardokae01@hotmail.com>
2023-07-17 17:34:38 -0300

    refactor: explicação

--- webStorage/webStorage.exemplos.js
+++ webStorage/webStorage.exemplos.js
@@ -3,4 +3,4 @@
 // localStorage.getItem('chave') => resgata uma entrada com a chave selecionada, se não existir retorna null
 // localStorage.removeItem('chave') => deleta a entrada com a chave selecionada
 // JSON.stringfy('objeto/array para transformar')
-// JSON.parse(localStorage.getItem('chave')) => transforma o obj/array transformado em string de volta ao estado original
+// JSON.parse(localStorage.getItem('chave')) => transforma a string obj/array de volta ao estado original
